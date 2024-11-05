693b60b6e1128736a7b44d91f4e95f67b9efd9e3
DEV - DoorSoft <dev2@doorsoft.com.br>
2024-11-05 09:02:39 -0300

    0.1

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu App Bar"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+         
+        children: <Widget>[
+          Text("item1"),
+          Text("item2"),
+          Text("item3"),
+          Text("item4"),
+          Text("item5"),
+          Text("item6"),
+        ],
+      )
+    );
+  }
+}
